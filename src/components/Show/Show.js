163abd61fd4bf5f88a89e6cfe499a8cd01530c7c
AnastasiaKonovalova <silentium901@mail.ru>
2018-12-27 18:20:31 +0300

    3* homework done

---
+++ src/components/Show/Show.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './Show.css';
+import {getShowInfo} from '../../api.js'
+
+export default class Show extends Component {
+    state = {
+        showId: '',
+        data: ''
+    }
+
+    static getDerivedStateFromProps(props, state){
+        return state = {showId: props.showId}
+    }
+
+    componentDidUpdate(){
+        getShowInfo(this.state.showId)
+            .then(response => this.setState( {data: response} ))
+    }
+
+    render(){
+        let content;
+        const {data} = this.state;
+        this.state.data !== ''
+        ? (
+            content = <div className='show'>
+                        <img className='show-image' alt={this.state.showId} src={data.image.medium}/>
+                        <h2 className='show-label t-show-name'>{data.name}</h2>
+                        <p className='show-text t-show-genre'><b>Жанр: </b>{data.genres.join(', ')}</p>
+                        <p className='show-text t-show-summary' dangerouslySetInnerHTML = {{__html: data.summary}}></p>
+                      </div>
+        ) : (
+            content = <p className='show-inforation t-show-info'>Шоу не выбрано</p>
+        )
+        return (
+            <div>
+                {content}
+            </div>
+        )
+    }
+}
