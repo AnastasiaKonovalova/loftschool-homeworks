48393a847d4a1cfe98a3f922b9d1a4e8388a12f8
AnastasiaKonovalova <silentium901@mail.ru>
2019-01-06 13:44:53 +0300

    3* homework final

--- src/components/Show/Show.js
+++ src/components/Show/Show.js
@@ -12,18 +12,21 @@ export default class Show extends Component {
         return state = {showId: props.showId}
     }
 
-    componentDidUpdate(){
-        getShowInfo(this.state.showId)
+    componentDidUpdate(prevProps){
+        const {showId} = this.state;
+        if(prevProps.showId !== showId){
+            getShowInfo(showId)
             .then(response => this.setState( {data: response} ))
+        }
     }
 
     render(){
         let content;
-        const {data} = this.state;
-        this.state.data !== ''
+        const {data, showId} = this.state;
+        data !== ''
         ? (
             content = <div className='show'>
-                        <img className='show-image' alt={this.state.showId} src={data.image.medium}/>
+                        <img className='show-image' alt={showId} src={data.image.medium}/>
                         <h2 className='show-label t-show-name'>{data.name}</h2>
                         <p className='show-text t-show-genre'><b>Жанр: </b>{data.genres.join(', ')}</p>
                         <p className='show-text t-show-summary' dangerouslySetInnerHTML = {{__html: data.summary}}></p>
