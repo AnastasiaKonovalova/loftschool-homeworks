bc0db03956a1d95100039b37d22d8c0ffd966096
AnastasiaKonovalova <silentium901@mail.ru>
2019-01-11 14:25:49 +0300

    7 homework in progress

--- src/reducers/orders.js
+++ src/reducers/orders.js
@@ -2,8 +2,23 @@ import { CREATE_NEW_ORDER } from '../modules/clients';
 import { MOVE_ORDER_NEXT, MOVE_ORDER_BACK } from '../actions/moveOrder';
 import { ADD_INGREDIENT } from '../actions/ingredients';
 
+// Реализуйте редьюсер
+// Типы экшенов, которые вам нужно обрабатывать уже импортированы
+// Обратите внимание на `orders.test.js`.
+// Он поможет понять, какие значения должен возвращать редьюсер.
+
 export default (state = [], action) => {
   switch (action.type) {
+    case CREATE_NEW_ORDER:
+      console.log('CREATE_NEW_ORDER', action.payload);
+      const newState = {
+        id: action.payload.id,
+        recipe: action.payload.recipe,
+        ingredients: [],
+        position: 'clients'
+      }
+      console.dir(newState)
+      return state;
     default:
       return state;
   }
