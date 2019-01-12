a8c48d696961f548a2557c71de40a2e41b5f7b0b
AnastasiaKonovalova <silentium901@mail.ru>
2019-01-12 12:59:15 +0300

    7 homework done

--- src/reducers/orders.js
+++ src/reducers/orders.js
@@ -10,15 +10,39 @@ import { ADD_INGREDIENT } from '../actions/ingredients';
 export default (state = [], action) => {
   switch (action.type) {
     case CREATE_NEW_ORDER:
-      console.log('CREATE_NEW_ORDER', action.payload);
-      const newState = {
+      const order = {
         id: action.payload.id,
         recipe: action.payload.recipe,
         ingredients: [],
         position: 'clients'
       }
-      console.dir(newState)
-      return state;
+      return state.concat([order]);
+
+    case MOVE_ORDER_NEXT:
+      const movedOrderNext = { ...state.find(order => order.id === action.payload) };
+
+      if(movedOrderNext.position === 'clients'){
+        movedOrderNext.position = 'conveyor_1'
+      } else if(movedOrderNext.position !== 'conveyor_4'){
+        movedOrderNext.position = `conveyor_${+movedOrderNext.position.slice(-1) + 1}`
+      } else if(movedOrderNext.ingredients.length > 0){
+        movedOrderNext.position = 'finish'
+      }
+      return state.map((order) => order.id === movedOrderNext.id ? movedOrderNext : order);
+    
+    case MOVE_ORDER_BACK:
+      const movedOrderBack = { ...state.find(order => order.id === action.payload) };
+
+      if(movedOrderBack.position !== 'conveyor_1' && movedOrderBack.position !== 'finish'){
+        movedOrderBack.position = `conveyor_${+movedOrderBack.position.slice(-1) - 1}`
+      } 
+      return state.map((order) => order.id === movedOrderBack.id ? movedOrderBack : order);
+
+    case ADD_INGREDIENT:
+      const modifiedOrder = { ...state.find(order => order.position === action.payload.from) };
+      modifiedOrder.ingredients = [...modifiedOrder.ingredients, action.payload.ingredient]
+
+      return state.map((order) => order.id === modifiedOrder.id ? modifiedOrder : order);
     default:
       return state;
   }
