7679dbb1f5c958bb2d86ba598d2fb53e69486dae
AnastasiaKonovalova <silentium901@mail.ru>
2019-01-17 11:58:57 +0300

    8 homework finish

--- src/store.js
+++ src/store.js
@@ -4,6 +4,6 @@ import searchMiddleware from './middlewares/searchMiddleware';
 import showMiddleware from './middlewares/showMiddleware';
 
 const getStore = () => createStore(reducer, 
-    window.__REDUX_DEVTOOLS_EXTENSION__(), 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), 
     applyMiddleware(searchMiddleware, showMiddleware))
 export default getStore
